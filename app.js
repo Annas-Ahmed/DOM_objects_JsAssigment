c8da1926dad76f5a256e80299899083e4770bc59
Annas-Ahmed <annasahmed1609@gmail.com>
2022-06-04 11:20:10 +0500

    DOM task COmplete

---
+++ app.js
@@ -0,0 +1,41 @@
+// question1
+// mainContent=document.getElementById("main-content")
+// console.log(mainContent.childNodes)
+render = document.getElementsByClassName("render")
+// console.log(render[0].innerHTML)
+
+// for (var i=0;i<render.length;i++){
+//     console.log(render[i].innerHTML)
+// }
+
+// firstName=document.getElementById("first-name")
+// firstName.value="annas"
+
+
+// firstName=document.getElementById("last-name")
+// firstName.value="AHMED"
+
+
+// firstName=document.getElementById("email")
+// firstName.value="annas@gmail.com"
+
+// question2
+
+// formContent=document.getElementById("form-content")
+// console.log(typeof formContent)
+
+// lastName=document.getElementById("lastName")
+// console.log(typeof lastName)
+// console.log(typeof lastName.childNodes)
+
+// mainContent=document.getElementById("main-content")
+// console.log(mainContent.firstChild)
+// console.log(mainContent.lastChild)
+
+// console.log(lastName.nextSibling)
+// console.log(lastName.previousSibling)
+
+// email=document.getElementById("email")
+// console.log(email.parentNode)
+// console.log(typeof email.parentNode)
+
